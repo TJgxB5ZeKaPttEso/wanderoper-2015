85a72f6a49d0446087c2ee1301875484c3b95f65
TJgxB5ZeKaPttEso <git@johanneskrohn.de>
2015-09-17 00:53:20 +0200

    Initial commit
    Signed-off-by: TJgxB5ZeKaPttEso <git@johanneskrohn.de>

---
+++ Gruntfile.js
@@ -0,0 +1,147 @@
+'use strict';
+module.exports = function (grunt) {
+
+    // load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+        // watch for changes and trigger sass, jshint, uglify and livereload
+
+        watch: {
+
+            sass: {
+                options: {
+                    livereload: true,
+                },
+                files: ['sass/**/*.{scss,sass}'],
+                tasks: ['sass', 'autoprefixer', 'cssmin']
+            },
+            js: {
+                files: '<%= jshint.all %>',
+                tasks: ['jshint', 'uglify']
+            },
+            images: {
+                files: ['images/**/*.{png,jpg,gif}'],
+                tasks: ['imagemin']
+            }
+        },
+
+        // sass
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded',
+                    require: 'susy'
+                },
+                files: {
+                    'sass/build/style.css': 'sass/style.scss',
+                    'sass/build/content-sidebar.css': 'sass/content-sidebar.scss',
+                    'sass/build/content-only.css': 'sass/content-only.scss',
+                }
+            }
+        },
+
+        // autoprefixer
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie 9', 'ios 6', 'android 4'],
+                map: true
+            },
+            files: {
+                expand: true,
+                flatten: true,
+                src: 'sass/build/*.css',
+                dest: 'sass/build'
+            },
+        },
+
+        // css minify
+        cssmin: {
+            options: {
+                keepSpecialComments: 1,
+                sourceMap: true
+            },
+            minify: {
+                expand: true,
+                cwd: 'sass/build',
+                src: ['*.css', '!*.min.css', '!content-sidebar.css','!content-only.css' ],
+                ext: '.css'
+            },
+            optionallayouts: {
+                expand: true,
+                cwd: 'sass/build',
+                src: ['content-sidebar.css', 'content-only.css'],
+                dest: 'layouts',
+                ext: '.css'
+            }
+        },
+
+        // javascript linting with jshint
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                "force": true
+            },
+            all: [
+                'Gruntfile.js',
+                'js/**/*.js'
+            ]
+        },
+
+        // uglify to concat, minify, and make source maps
+        uglify: {
+            plugins: {
+                options: {
+                    sourceMap: '/js/plugins.js.map',
+                    sourceMappingURL: 'plugins.js.map',
+                    sourceMapPrefix: 2
+                },
+                files: {
+                    'js/plugins.min.js': [
+                        'js/source/plugins.js',
+                        'js/vendor/navigation.js',
+                        'js/vendor/skip-link-focus-fix.js',
+                        // 'assets/js/vendor/yourplugin/yourplugin.js',
+                    ]
+                }
+            },
+            main: {
+                options: {
+                    sourceMap: 'js/main.js.map',
+                    sourceMappingURL: 'main.js.map',
+                    sourceMapPrefix: 2
+                },
+                files: {
+                    'js/main.min.js': [
+                        'js/source/main.js'
+                    ]
+                }
+            }
+        },
+
+        // image optimization
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 7,
+                    progressive: true,
+                    interlaced: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/'
+                }]
+            }
+        },
+
+
+    });
+
+    // rename tasks
+    // register task
+    grunt.registerTask('default', ['sass', 'autoprefixer', 'cssmin', 'uglify', 'imagemin', 'watch']);
+
+};
---
+++ footer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Wanderoper Brandenburg 2015
+ */
+
+?>
+
+	</div><!-- #content -->
+
+<!--livereload and typekit-->
+
+<script src="//localhost:35729/livereload.js"></script>
+
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wanderoper-2015' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'wanderoper-2015' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wanderoper-2015' ), 'wanderoper-2015', '<a href="http://johanneskrohn.de" rel="designer">Johannes Krohn</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
